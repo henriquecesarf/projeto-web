6a5652b9f26536e46d2b16a91caa9e87a514c08f
henriquecesarf <henriquecfdantas@gmail.com>
2024-09-21 23:35:17 -0300

    Initial commit

---
+++ src/main/java/com/seuprojeto/projeto_web/ProjetoWebApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.projeto_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seuprojeto/projeto_web/ProjetoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.projeto_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
