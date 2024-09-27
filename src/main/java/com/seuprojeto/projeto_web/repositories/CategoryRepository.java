2e34ec9051b711d3c7e8271cd020354dab72c670
henriquecesarf <henriquecfdantas@gmail.com>
2024-09-26 22:50:12 -0300

    feature/category

---
+++ src/main/java/com/seuprojeto/projeto_web/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.seuprojeto.projeto_web.repositories;
+
+import com.seuprojeto.projeto_web.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+    boolean existsByName(String name);
+}
