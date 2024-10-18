3ba73aad0aa2fea9c2ccaccaf49958e446e0183b
Digo2088 <55104167+Digo2088@users.noreply.github.com>
2024-10-18 12:06:53 -0300

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,24 @@
+-- inserção de dados automatica ao rodar a aplicação
+
+-- inserção de usuarios
+INSERT INTO users (id, name, privilege) 
+VALUES 
+(1, 'supervisor', 'admin');
+INSERT INTO users (id, name, privilege) 
+VALUES 
+(2, 'funcionario', 'user');
+
+-- inserção de sinistros
+INSERT INTO sinister (identification, gravity)
+VALUES 
+('Colisão frontal em rodovia', 'HIGH'),
+('Batida traseira leve em área urbana', 'LOW'),
+('Capotamento', 'MEDIUM'),
+('Atropelamento de pedestre', 'HIGH'),
+('Colisão lateral em cruzamento', 'MEDIUM');
+
+--inserção de opcionais
+INSERT INTO optional (name, value_location, value_declared, qtd_available) VALUES
+('Cadeirinha para Bebê', 35.00, 250.00, 4),
+('Assento Infantil', 20.00, 150.00, 7),
+('Caixa De Transporte de Animais', 30.00, 200.00, 2);
---
+++ target/classes/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    privilege VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sinister (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    identification VARCHAR(255) NOT NULL,
+    gravity ENUM('LOW', 'MEDIUM', 'HIGH') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS optional (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    value_location DECIMAL(10, 2) NOT NULL,
+    value_declared DECIMAL(10, 2) NOT NULL,
+    qtd_available INT NOT NULL
+);
