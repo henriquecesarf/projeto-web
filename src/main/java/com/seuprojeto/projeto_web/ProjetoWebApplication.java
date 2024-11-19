73eca041ba1d6d1c63b62426af41fa69111c2362
Robert Magalhães <145805081+roberttmag@users.noreply.github.com>
2024-11-19 09:39:03 -0300

    ConfigRedis

--- src/main/java/com/seuprojeto/projeto_web/ProjetoWebApplication.java
+++ src/main/java/com/seuprojeto/projeto_web/ProjetoWebApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @Slf4j
 public class ProjetoWebApplication {
---
+++ src/main/java/com/seuprojeto/projeto_web/cache/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.seuprojeto.projeto_web.cache;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Vehicle> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Vehicle> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+
+
+}
